3bebaa352d73d9ab31453298ad651a41e8ac8f8a
KateLy30 <kateshytova2006@gmail.com>
2025-12-19 20:50:58 +0300

    update

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace customer_request_accounting_system
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
