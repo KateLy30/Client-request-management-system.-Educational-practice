bc6d84dfc9682e712ce5bee3969e719938bfa582
KateLy30 <kateshytova2006@gmail.com>
2025-12-19 21:01:33 +0300

    update

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "customer_request_accounting_system")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "customer_request_accounting_system.Pages")]
